3be6f6c71743606c6bb3a9d318479943af92722f
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-09-28 23:19:02 +0530

    Added UC5 : Calculated Wages for a month

--- src/EmpWage/EmployeeWage.java
+++ src/EmpWage/EmployeeWage.java
@@ -1,29 +1,32 @@
 package EmpWage;
 
 public class EmployeeWage {
-
     static final int WAGE_PER_HOUR=20;
     static final int FULL_TIME_HOUR=8;
     static final int PART_TIME_HOUR=4;
     static final int IS_FULL_TIME=2;
     static final int IS_PART_TIME=1;
+    static final int WORKING_DAYS_PER_MONTH=20;
+
     public static void main(String[] args) {
 
-        int employeeAttendence=(int)Math.floor(Math.random()*10)%3;
-        System.out.println(employeeAttendence);
-        int dailyWage=0;
-        switch(employeeAttendence) {
-            case IS_FULL_TIME:
-                System.out.println("Employee is Full Time");
-                dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Part Time");
-                dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                break;
-            default:
-                System.out.println("Employee is absent");
-        }
+        int day=1;
+        int totalWages=0;
+        while(day<=WORKING_DAYS_PER_MONTH) {
+            int dailyWage=0;
+            int employeeAttendence=(int)Math.floor(Math.random()*10)%3;
+            switch (employeeAttendence) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Full Time");
+                    dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Part Time");
+                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+            }
 //        if(employeeAttendence==2){
 //            System.out.println("Employee is full time");
 //            dailyWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
@@ -35,6 +38,10 @@ public class EmployeeWage {
 //        else{
 //              System.out.println("Employee is absent");
 //        }
-        System.out.println("Daily Wage of employee is => "+dailyWage);
+            totalWages+=dailyWage;
+            System.out.println("Daily Wage of employee for day "+day+" is => " + dailyWage);
+            day++;
+        }
+        System.out.println("Tatal Wages of employee is => "+totalWages);
     }
 }
