7bf7f85f28c6c1b229a2b624a7238786937257a3
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-09-28 22:35:26 +0530

    Added UC3 : Added part time & employee wage

--- src/EmpWage/EmployeeWage.java
+++ src/EmpWage/EmployeeWage.java
@@ -4,15 +4,20 @@ public class EmployeeWage {
 
     static final int WAGE_PER_HOUR=20;
     static final int FULL_TIME_HOUR=8;
+    static final int PART_TIME_HOUR=4;
 
     public static void main(String[] args) {
 
-        int employeeAttendence=(int)Math.floor(Math.random()*10)%2;
+        int employeeAttendence=(int)Math.floor(Math.random()*10)%3;
         System.out.println(employeeAttendence);
         int dailyWage=0;
-        if(employeeAttendence==1){
-            System.out.println("Employee is present");
+        if(employeeAttendence==2){
+            System.out.println("Employee is full time");
             dailyWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
+        }
+        else if(employeeAttendence==1){
+            System.out.println("Employee is part time");
+            dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
         }
         else{
             System.out.println("Employee is absent");
