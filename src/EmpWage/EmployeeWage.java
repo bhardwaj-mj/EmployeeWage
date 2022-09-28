e6d0705e57c1af4fcd85a19d3739bbc1c81d8d53
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-09-28 23:33:20 +0530

    Added UC6 : Calculating wages till a condition of total working hours or days is reached for a month

--- src/EmpWage/EmployeeWage.java
+++ src/EmpWage/EmployeeWage.java
@@ -2,27 +2,32 @@ package EmpWage;
 
 public class EmployeeWage {
     static final int WAGE_PER_HOUR=20;
-    static final int FULL_TIME_HOUR=8;
-    static final int PART_TIME_HOUR=4;
+    static final int FULL_TIME_HOUR=12;
+    static final int PART_TIME_HOUR=6;
     static final int IS_FULL_TIME=2;
     static final int IS_PART_TIME=1;
     static final int WORKING_DAYS_PER_MONTH=20;
+    static final int WORKING_HOURS_PER_MONTH=100;
+
 
     public static void main(String[] args) {
 
         int day=1;
         int totalWages=0;
-        while(day<=WORKING_DAYS_PER_MONTH) {
+        int empHours=0;
+        while(day<=WORKING_DAYS_PER_MONTH && empHours<=WORKING_HOURS_PER_MONTH) {
             int dailyWage=0;
             int employeeAttendence=(int)Math.floor(Math.random()*10)%3;
             switch (employeeAttendence) {
                 case IS_FULL_TIME:
                     System.out.println("Employee is Full Time");
                     dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+                    empHours=empHours+FULL_TIME_HOUR;
                     break;
                 case IS_PART_TIME:
                     System.out.println("Employee is Part Time");
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    empHours=empHours+PART_TIME_HOUR;
                     break;
                 default:
                     System.out.println("Employee is absent");
@@ -42,6 +47,7 @@ public class EmployeeWage {
             System.out.println("Daily Wage of employee for day "+day+" is => " + dailyWage);
             day++;
         }
+        System.out.println("Total working hours of employee is => "+empHours);
         System.out.println("Tatal Wages of employee is => "+totalWages);
     }
 }
